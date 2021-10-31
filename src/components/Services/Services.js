da93bb86df448719b896941fb14b03d309576f2a
borshon420 <borshonarafat420@gmail.com>
2021-10-31 20:33:33 +0600

    spinner added on the services section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
+import useAuth from "../../hooks/useAuth";
 import Service from "../Service/Service";
 import "./Services.css";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const {isLoading} = useAuth();
+  
   useEffect(() => {
     fetch("https://nameless-brook-91912.herokuapp.com/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
+  if (isLoading){
+    return <Spinner className="mt-5" animation="border" />
+  }
   return (
     <div>
       
